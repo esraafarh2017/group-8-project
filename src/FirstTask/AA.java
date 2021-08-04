48978981ecafb1b421cbbb0163df74dfd91bdcfd
aysu <aysuahmadli@gmail.com>
2021-08-04 12:50:39 -0400

    second commit

--- src/FirstTask/AA.java
+++ src/FirstTask/AA.java
@@ -3,5 +3,6 @@ package FirstTask;
 public class AA {
     public static void main(String[] args) {
         System.out.println("Here is my code, Aysu wrote this code");
+        System.out.println("Some more code here");
     }
 }
