4b24ea98b69a52ac8e750708407af7e420196b55
rmautia <raphaelnyangenya@gmail.com>
2021-08-04 19:58:20 +0300

    Happy

--- src/FirstTask/Raphael.java
+++ src/FirstTask/Raphael.java
@@ -4,5 +4,6 @@ public class Raphael {
     public static void main(String[] args) {
         System.out.println("Hello Team");
         System.out.println("Glad we are a team");
+        System.out.println("Yaay");
     }
 }
