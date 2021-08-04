9634015359b366841e4344eb5e5efa6a950f8d6e
aysu <aysuahmadli@gmail.com>
2021-08-04 12:51:57 -0400

    completed

--- src/FirstTask/AA.java
+++ src/FirstTask/AA.java
@@ -4,5 +4,6 @@ public class AA {
     public static void main(String[] args) {
         System.out.println("Here is my code, Aysu wrote this code");
         System.out.println("Some more code here");
+        System.out.println("completed");
     }
 }
