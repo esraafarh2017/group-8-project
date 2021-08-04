c15e3d85d6b8a28958c9ce40b8bb1fc785eadfe9
inesa <inesa.yun@yahoo.com>
2021-08-04 12:52:15 -0400

    wrote some code

--- src/FirstTask/FirstClass.java
+++ src/FirstTask/FirstClass.java
@@ -4,6 +4,10 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("Hello group 8");
+
+        int a = 5;
+        int b = 6;
+        int c = a+b;
     }
 
 }
