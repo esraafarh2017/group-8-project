6de7010eb0b4676c344ca7f50f7e79ea72f97bdc
inesa <inesa.yun@yahoo.com>
2021-08-04 13:00:09 -0400

    wrote some code

--- src/FirstTask/FirstClass.java
+++ src/FirstTask/FirstClass.java
@@ -3,12 +3,8 @@ package FirstTask;
 public class FirstClass {
 
     public static void main(String[] args) {
-
-        System.out.println("Hello group 8");
-
-        int a = 5;
-        int b = 6;
-        int c = a+b;
+        //Inesa
     }
+
 
 }
--- src/FirstTask/IY.java
+++ src/FirstTask/IY.java
@@ -1,10 +1,9 @@
 package FirstTask;
 
-import java.sql.SQLOutput;
-
 public class IY {
 
     public static void main(String[] args) {
+
         System.out.println("Hello group 8");
     }
 
